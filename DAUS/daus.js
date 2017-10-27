f37f6e1f9814db06162c914ca754d66f5f0b1175
Lavjit Singh <ls2638@cirvianum.cat>
2017-10-27 20:02:09 +0200

    Amb frequencia

--- DAUS/daus.js
+++ DAUS/daus.js
@@ -4,27 +4,20 @@ acumularà el nombre de vegades que ha sortit cada cara del dau (tot i que encar
 de moment, que ho feu amb variables). Una vegada ho tingueu, feu una còpia de seguretat del codi fet i modifiqueu-lo per tal que el dau estigui desequilibrat.
 En aquest cas, podeu decidir vosaltres quina cara del dau és la que està desequilibrada.*/
 
-var llançaments = 100;
-var cara1 = 0;
-var cara2 = 0;
-var cara3 = 0;
-var cara4 = 0;
-var cara5 = 0;
-var cara6 = 0;
+var numPlay = prompt("Quantes partides vols fer: ");
+     // 1  2  3  4  5 6
+var fr=[15,15,30,30,5,5];
+var minPointoWin = 5;
+var probtowin = 0;
+var sumafr = 0;
 
-for (var i = 0; i < llançaments; i++) {
-  var numDauAleatori = Math.random();
-  if (numDauAleatori < 0.15) {
-    cara1++;
-  } else if (numDauAleatori < 0.3) {
-    cara2++;
-  } else if (numDauAleatori < 0.6) {
-    cara3++;
-  } else if (numDauAleatori < 0.9) {
-    cara4++;
-  } else if (numDauAleatori < 0.95) {
-    cara5++;
-  } else if (numDauAleatori < 1) {
-    cara6++;
-  }
+
+// suma dels elements del arrays
+for (var i = 0; i < fr.length; i++){
+  sumafr += fr[i];
+}
+for (var i = 0; i < minPointoWin -1; i++){
+  probtowin = fr[i] / sumafr;
+  document.write("<br>");
+  document.write(probtowin);
 }
