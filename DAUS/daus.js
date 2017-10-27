a6ef9e694eee02101a58a014d5cf5fd76a7e5c0d
Lavjit Singh <ls2638@cirvianum.cat>
2017-10-27 20:49:51 +0200

    dau acabat

--- DAUS/daus.js
+++ DAUS/daus.js
@@ -7,9 +7,11 @@ En aquest cas, podeu decidir vosaltres quina cara del dau és la que està deseq
 var numPlay = prompt("Quantes partides vols fer: ");
      // 1  2  3  4  5 6
 var fr=[15,15,30,30,5,5];
-var minPointoWin = 5;
+var minPointoWin = 4;
+var probtolose = 0;
 var probtowin = 0;
 var sumafr = 0;
+var winmatch = 0;
 
 
 // suma dels elements del arrays
@@ -17,7 +19,28 @@ for (var i = 0; i < fr.length; i++){
   sumafr += fr[i];
 }
 for (var i = 0; i < minPointoWin -1; i++){
-  probtowin = fr[i] / sumafr;
+  probtolose+= fr[i];
   document.write("<br>");
-  document.write(probtowin);
 }
+probtolose = probtolose / sumafr;
+probtowin = (sumafr / sumafr) - probtolose;
+document.getElementById("probtowin").innerHTML = probtowin;
+
+for(var i = 0; i < numPlay; i++){
+    var numrand = Math.random();
+    if(numrand >= probtolose && numrand <=1){
+        winmatch++;
+    }
+}
+
+var moneylose = numPlay * 20;
+var moneywin = winmatch * 100;
+
+
+if(moneywin > moneylose){
+    document.getElementById("win").innerHTML =("Ha guanyat " + winmatch + " partits en " + numPlay + " tirades i a guanyat " + moneywin + "€");
+
+}else{
+   document.getElementById("lose").innerHTML =("Ha guanyat " + winmatch + " partits en " + numPlay + " tirades i a perdut " + moneylose + "€");
+}
+
